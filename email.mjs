56519a61e7f25a1940b1af1ae0ab20c18f60468c
Abigail Asante <asantea331@gmail.com>
2024-06-26 09:07:58 +0000

    Sending emails in node.js

---
+++ email.mjs
@@ -0,0 +1,42 @@
+
+
+// require is not applicable in front end programing therefore using import puts you on the safer side.
+// const {program} = require ('commnder')
+
+import { program } from 'commander'
+import { createTransport } from 'nodemailer';
+
+// create email transport
+const transporter = createTransport({
+    host: 'smtppro.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "admin@shopa.life",
+        pass: "KqV7XsXwn5DK"
+    }
+})
+
+program
+    .option('-s', '--subject')
+    .option('-t', '--to')
+    .option('-m', '--message');
+
+program.parse();
+
+const options = program.opts();
+console.log(program.args);
+console.log(options);
+if (options.s && options.t && options.m) {
+    // send email
+    transporter.sendMail({
+        from: 'noreply@shopa.life',
+        to: program.args[1],
+        subject: program.args[0],
+        text: program.args[2]
+    })
+        .then(console.log)
+        .catch(console.log);
+} else {
+    console.log('send options are missing');
+}
